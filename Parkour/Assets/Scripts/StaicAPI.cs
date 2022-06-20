8134985ca6b8d76a525c97c9c90e1e4e0a9c2eb0
YANGSONGJONG <105775327+YANGSONGJONG@users.noreply.github.com>
2022-06-20 10:11:44 +0800

    Update 練習靜態API

--- Parkour/Assets/Scripts/StaicAPI.cs
+++ Parkour/Assets/Scripts/StaicAPI.cs
@@ -8,6 +8,8 @@ namespace YANGSONGJING
     /// </summary>
     public class StaicAPI : MonoBehaviour
     {
+        private Vector3 a = new Vector3(1, 1, 1);
+        private Vector3 b = new Vector3(22, 22, 22);
         private void Start()
         {
             // 取得靜態屬性 get
@@ -26,17 +28,31 @@ namespace YANGSONGJING
 
             // 使用靜態方法
             // 語法 :
-            // 類別.靜態方法名稱(對應引數)
+            // 類別.靜態方法名稱
             float range = Random.Range(20.5f, 100.5f);
             print("隨機範圍 : " + range);
-        
+            print("Camera 數量:"+Camera.allCamerasCount);
+            print("目前的平台 :" + Application.platform);
+            Physics.sleepThreshold = 10;
+            print("睡眠臨界值 :" + Physics.sleepThreshold);
+            Time.timeScale = 0.5f;
+            print("時間大小 :" + Time.timeScale);
+            // 類別.靜態方法名稱(對應引數)
+            print("9.999 去掉小數點四捨五入:" + Mathf.Round(9.999f));
+
+            float distance = Vector3.Distance(a,b);
+            print("<color=yellow>距離: " + distance + "</color>");
+            Application.OpenURL("https://unity.com/");
         }
-        
+
         private void Update()
         {
             int rangeint = Random.Range(1, 3);
             print("隨機範圍 : " + rangeint);
+            print("<color=red>是否按下空白鍵 :" + Input.GetKeyDown(KeyCode.Space) + "</color>");
+
         }
+        
         
 
     }
